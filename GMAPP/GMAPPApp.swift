52d7331084816ceb20031910925af8c42b3cea58
LSWarss <ls.warss98@gmail.com>
2021-02-27 16:52:16 +0100

    Initial Commit

---
+++ GMAPP/GMAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  GMAPPApp.swift
+//  GMAPP
+//
+//  Created by Lukasz Stachnik on 27/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct GMAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
