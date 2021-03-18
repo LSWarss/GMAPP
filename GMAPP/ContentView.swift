9ce0a63e0a0a3614d67dde1581e46041e267daea
LSWarss <ls.warss98@gmail.com>
2021-03-18 18:04:51 +0100

    Initial Commit

---
+++ GMAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GMAPP
+//
+//  Created by Lukasz Stachnik on 18/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GMAPP/GMAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  GMAPPApp.swift
+//  GMAPP
+//
+//  Created by Lukasz Stachnik on 18/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct GMAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
