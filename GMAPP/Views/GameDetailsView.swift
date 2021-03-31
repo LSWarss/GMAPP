6eada917bf9a69149c76573163d03c9971054645
LSWarss <ls.warss98@gmail.com>
2021-03-31 18:52:12 +0200

    First impl of new api format, and some tweaks to GameDetailsView

--- GMAPP/Views/GameDetailsView.swift
+++ GMAPP/Views/GameDetailsView.swift
@@ -8,20 +8,40 @@
 import SwiftUI
 
 struct GameDetailsView: View {
-
+    
     var game : Game
-
+    
     var body: some View {
-        ZStack{
+        ZStack {
             VStack {
                 Text(game.title)
                     .font(.largeTitle)
-                Text(game.platform)
-                Text(Helpers.postgresDateToDateConverter(from: game.formattedDate), style: .date)
-                Helpers.releaseDaysText(game: game)
-                
-            }
-        }.edgesIgnoringSafeArea(.all)
+                    .padding(10)
+                ZStack(alignment: .top ){
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(Color("OffWhite"))
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y:10)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                    VStack{
+                        Text(game.platform)
+                            .padding(.top, 20)
+                        Text(Helpers.postgresDateToDateConverter(from: game.formattedDate), style: .date)
+                        Helpers.releaseDaysText(game: game)
+                        Text(game.description)
+                            .padding()
+                            .font(.body)
+                        HStack() {
+                            Text("Users score: ")
+                            Text(game.formattedScore)
+                                .bold()
+                        }
+                    }
+                    .padding()
+                }.ignoresSafeArea()
+            }.padding(.top, 100)
+        }
+        .background(Color("OffWhite"))
+        .ignoresSafeArea()
     }
 }
 
