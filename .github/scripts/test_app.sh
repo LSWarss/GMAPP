ae08f97710c42e83d796e6b7e381bfdfbc62c63e
LSWarss <ls.warss98@gmail.com>
2021-05-25 19:13:01 +0200

    Trying out some github actiosn for proejct

---
+++ .github/scripts/test_app.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -eo pipefail
+
+xcodebuild -workspace GMAPP.xcworkspace \
+            -scheme GMAPP
+            -destination platform=iOS\ Simulator,OS=14.5,name=iPhone\ 12 \
+            clean test | xcpretty
