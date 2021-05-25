baa1eaddbbb969757a935a6b35e56d88bcd711a6
LSWarss <ls.warss98@gmail.com>
2021-05-25 19:25:46 +0200

    Trying out to get good syntax on action

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 xcodebuild -workspace GMAPP.xcworkspace \
-            -scheme GMAPP
+            -scheme GMAPP \
             -destination platform=iOS\ Simulator,OS=14.5,name=iPhone\ 12 \
             clean test | xcpretty
